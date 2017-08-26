7e9cb8ea8c9b6e677ba8fe83a647ea671e9b394c
shuaigood <shuaigood@163.com>
2017-08-26 10:27:01 +0800

    first create

---
+++ WirelessControl/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Management;
+
+namespace WirelessControl
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string T61_WIFI_ADAPTER = "Intel(R) PRO/Wireless 3945ABG Network Connection";
+
+        private ManagementObject m_WifiAdapter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private ManagementObject GetWifiAdapter(string name)
+        {
+            //Intel(R) PRO/Wireless 3945ABG Network Connection
+
+            ManagementObject objFound = null;
+
+            string manage = "SELECT * From Win32_NetworkAdapter";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(manage);
+            ManagementObjectCollection collection = searcher.Get();
+            //List<string> netWorkList = new List<string>();
+
+            foreach (ManagementObject obj in collection)
+            {
+                string objName = obj["Name"].ToString();
+                if (objName.Equals(name))
+                {
+                    objFound = obj;
+                    break;
+                    //netWorkList.Add(objName);
+                    //Console.WriteLine(objName);
+                }
+            }
+
+            return objFound;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            m_WifiAdapter = GetWifiAdapter(T61_WIFI_ADAPTER);
+        }
+
+        private void Disable_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                m_WifiAdapter.InvokeMethod("Disable", null);
+            }
+            catch
+            {
+            }
+
+        }
+
+        private void Enable_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                m_WifiAdapter.InvokeMethod("Enable", null);
+            }
+            catch
+            {
+            }
+        }
+
+
+    }
+}
---
+++ csapp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+
+namespace csapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Intel(R) PRO/Wireless 3945ABG Network Connection
+
+            string manage = "SELECT * From Win32_NetworkAdapter";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(manage);
+            ManagementObjectCollection collection = searcher.Get();
+            List<string> netWorkList = new List<string>();
+
+            foreach (ManagementObject obj in collection)
+            {
+                string objName = obj["Name"].ToString();
+                if (objName.Equals("Intel(R) PRO/Wireless 3945ABG Network Connection"))
+                {
+                    netWorkList.Add(objName);
+                    Console.WriteLine(objName);
+                }
+            }
+            //this.cmbNetWork.DataSource = netWorkList;
+
+
+
+            Console.Read();
+        }
+    }
+}
